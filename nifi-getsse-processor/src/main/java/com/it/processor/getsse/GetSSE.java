7f719201a4e0e1b0496f4b5c7ea800659b2b32b6
Daniel <dakiethkinson@gmail.com>
2024-04-22 19:02:51 -0600

    Updates processor to manage two relationships. Starts work to pass flowfile to relationships

--- nifi-getsse-processor/src/main/java/com/it/processor/getsse/GetSSE.java
+++ nifi-getsse-processor/src/main/java/com/it/processor/getsse/GetSSE.java
@@ -18,18 +18,18 @@ package com.it.processor.getsse;
 
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.sse.InboundSseEvent;
 import jakarta.ws.rs.sse.SseEventSource;
 import org.apache.nifi.annotation.behavior.*;
+import org.apache.nifi.annotation.lifecycle.OnEnabled;
+import org.apache.nifi.annotation.lifecycle.OnScheduled;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.flowfile.FlowFile;
-import org.apache.nifi.annotation.lifecycle.OnScheduled;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
-import org.apache.nifi.annotation.documentation.SeeAlso;
 import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processor.AbstractProcessor;
@@ -41,21 +41,29 @@ import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.util.StandardValidators;
 
 import java.util.*;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicReference;
 
 @TriggerWhenEmpty
-@Tags({"example"})
-@CapabilityDescription("Provide a description")
-@SeeAlso({})
-@ReadsAttributes({@ReadsAttribute(attribute="asdf", description="asdf")})
-@WritesAttributes({@WritesAttribute(attribute="asdf", description="asdf")})
+@Tags({"SSE, Serve Sent Event"})
+@CapabilityDescription("Provides an GET Endpoint for Server Sent Events.")
+@ReadsAttributes({@ReadsAttribute(attribute="none", description="Does not Read Attributes")})
+@WritesAttributes({@WritesAttribute(attribute="Nothing yet...", description="TODO")})
 public class GetSSE extends AbstractProcessor {
 
     public static final PropertyDescriptor URL = new PropertyDescriptor
             .Builder().name("URL")
-            .displayName("url")
-            .description("URL of the SSE emitter")
+            .displayName("URL")
+            .description("URL of the SSE API emitter")
             .required(true)
             .addValidator(StandardValidators.URL_VALIDATOR)
+            .build();
+
+    public static final PropertyDescriptor API_KEY = new PropertyDescriptor
+            .Builder().name("API_KEY")
+            .displayName("TODO: API Key")
+            .description("TODO: API Key of the SSE API emitter")
+            .required(false)
             .build();
 
     public static final PropertyDescriptor BATCH_SIZE = new PropertyDescriptor.Builder()
@@ -66,24 +74,9 @@ public class GetSSE extends AbstractProcessor {
             .defaultValue("10")
             .build();
 
-    public static final Relationship ORIGINAL = new Relationship.Builder()
-            .name("Original")
-            .description("Request FlowFiles transferred when receiving HTTP responses with a status code between 200 and 299.")
-            .build();
-
     public static final Relationship RESPONSE = new Relationship.Builder()
             .name("Response")
             .description("Response FlowFiles transferred when receiving HTTP responses with a status code between 200 and 299.")
-            .build();
-
-    public static final Relationship RETRY = new Relationship.Builder()
-            .name("Retry")
-            .description("Request FlowFiles transferred when receiving HTTP responses with a status code between 500 and 599.")
-            .build();
-
-    public static final Relationship NO_RETRY = new Relationship.Builder()
-            .name("No Retry")
-            .description("Request FlowFiles transferred when receiving HTTP responses with a status code between 400 an 499.")
             .build();
 
     public static final Relationship FAILURE = new Relationship.Builder()
@@ -93,26 +86,23 @@ public class GetSSE extends AbstractProcessor {
 
     private List<PropertyDescriptor> properties;
     private Set<Relationship> relationships;
-
+    private Queue<InboundSseEvent> queue = new LinkedBlockingDeque<>();
     /*
     HERE BE BUGS
      */
 
-//    @Override
-//    protected void init(final ProcessorInitializationContext context){
-//        final List<PropertyDescriptor> properties = new ArrayList<>();
-//        properties.add(URL);
-//        properties.add(BATCH_SIZE);
-//        this.properties = Collections.unmodifiableList(this.properties);
-//
-//        final Set<Relationship> relationships = new HashSet<>();
-//        relationships.add(ORIGINAL);
-//        relationships.add(RESPONSE);
-//        relationships.add(RETRY);
-//        relationships.add(NO_RETRY);
-//        relationships.add(FAILURE);
-//        this.relationships = Collections.unmodifiableSet(relationships);
-//    }
+    @Override
+    protected void init(final ProcessorInitializationContext context){
+        properties = new ArrayList<>();
+        properties.add(URL);
+        properties.add(BATCH_SIZE);
+        this.properties = Collections.unmodifiableList(this.properties);
+
+        relationships = new HashSet<>();
+        relationships.add(RESPONSE);
+        relationships.add(FAILURE);
+        this.relationships = Collections.unmodifiableSet(relationships);
+    }
 
     @Override
     protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
@@ -137,24 +127,35 @@ public class GetSSE extends AbstractProcessor {
         final ComponentLog log = getLogger();
 
         ObjectMapper attributes = new ObjectMapper();
-
         FlowFile flowFile = session.get();
-        FlowFile responseFile = null;
+
         try{
-            if (flowFile == null) {
-                try (SseEventSource source = SseEventSource.target(target).build()) {
-                    source.register(inboundSseEvent -> {
-                        try{
-                            session.putAllAttributes(responseFile, attributes.readValue(inboundSseEvent.readData(), Map.class));
-                        } catch (JsonProcessingException e) {
-                            log.error(e.getMessage());
-                        }
-                    });
-                    source.open();
-                }
+            try (SseEventSource source = SseEventSource.target(target).build()) {
+                source.open();
+                source.register(inboundSseEvent -> {
+                    queue.add(inboundSseEvent);
+                    handleEventQueue(batchSize, session, flowFile);
+                });
             }
         }catch (final Exception e){
             log.error(e.getMessage());
+            session.putAttribute(flowFile, "error", e.getMessage());
+            session.transfer(flowFile, FAILURE);
+        }
+    }
+
+    public void handleEventQueue(int batchSie, final ProcessSession session, FlowFile flowFile){
+        if(queue.size() >= batchSie){
+            for(int i = 0; i < batchSie; i++){
+                InboundSseEvent inboundSseEvent = queue.poll();
+                session.write(flowFile, outputStream -> {
+                    outputStream.write(inboundSseEvent.getId().getBytes());
+                    outputStream.write(inboundSseEvent.getName().getBytes());
+                    outputStream.write(inboundSseEvent.getComment().getBytes());
+                    outputStream.write(inboundSseEvent.readData().getBytes());
+                });
+                session.transfer(flowFile, RESPONSE);
+            }
         }
     }
 }
